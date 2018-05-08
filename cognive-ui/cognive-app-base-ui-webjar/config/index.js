719577f810fab105e0842c6c91d5942ca799a56a
Denis <d.ermolovich@adhoc.by>
2018-05-08 17:53:16 +0300

    UI. Minor fixes.

--- cognive-ui/cognive-app-base-ui-webjar/config/index.js
+++ cognive-ui/cognive-app-base-ui-webjar/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/proto', // Assets path has been changed for Cognive project
+    assetsPublicPath: '/proto/', // Assets path has been changed for Cognive project
 
     /**
      * Source Maps
